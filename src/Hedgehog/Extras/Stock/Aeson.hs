967d79533c21e33387d0227a5f6cc185203fe658
Nicholas Clarke <nick@topos.org.uk>
2022-03-14 13:14:33 +0100

    Update to support aeson >= 2.0.0 (#8)
    Bump the version number here accordingly. `rewriteObject` could now
    probably take a nicer interface, but we retain the old one for
    compatiblity.

--- src/Hedgehog/Extras/Stock/Aeson.hs
+++ src/Hedgehog/Extras/Stock/Aeson.hs
@@ -3,12 +3,14 @@ module Hedgehog.Extras.Stock.Aeson
   ) where
 
 import           Data.Aeson
+import qualified Data.Aeson.KeyMap as KM
 import           Data.HashMap.Lazy
 import           Data.Text
+import           Prelude ((.), ($))
 
 -- | Rewrite a JSON object to another JSON object using the function 'f'.
 --
 -- All other JSON values are preserved.
 rewriteObject :: (HashMap Text Value -> HashMap Text Value) -> Value -> Value
-rewriteObject f (Object hm) = Object (f hm)
+rewriteObject f (Object hm) = Object (KM.fromHashMapText . f . KM.toHashMapText $ hm)
 rewriteObject _ v = v
