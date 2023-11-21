aa066740c825f5f9f3dc0e24c47fb1b5ae1ca4b4
Erik de Castro Lopo <erikd@mega-nerd.com>
2023-11-21 16:25:12 +1100

    Make it build with ghc-9.8
    Drop hw-aeson dependency on the way.

--- src/Hedgehog/Extras/Stock/Aeson.hs
+++ src/Hedgehog/Extras/Stock/Aeson.hs
@@ -4,18 +4,16 @@ module Hedgehog.Extras.Stock.Aeson
   ) where
 
 import           Data.Aeson
+import           Data.Aeson.KeyMap (KeyMap)
 import           Data.Functor
-import           Data.HashMap.Lazy
-import           Data.Text
-import           Prelude (($), (.))
 
 import qualified Data.Aeson.KeyMap as KM
 
 -- | Rewrite a JSON object to another JSON object using the function 'f'.
 --
 -- All other JSON values are preserved.
-rewriteObject :: (HashMap Text Value -> HashMap Text Value) -> Value -> Value
-rewriteObject f (Object hm) = Object (KM.fromHashMapText . f . KM.toHashMapText $ hm)
+rewriteObject :: (KeyMap Value -> KeyMap Value) -> Value -> Value
+rewriteObject f (Object hm) = Object (f hm)
 rewriteObject _ v = v
 
 -- | Rewrite each element of a JSON array using the function 'f'.
